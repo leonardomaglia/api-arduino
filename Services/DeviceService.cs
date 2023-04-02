99361cfa74bb1ecae7168006dca0fc4ca396236d
Leonardo Maglia <leonardoleonim@outlook.com>
2023-04-02 00:18:37 -0300

    Implementaçao abertura de conexão

--- Services/DeviceService.cs
+++ Services/DeviceService.cs
@@ -1,4 +1,5 @@
 using api_arduino.Interfaces;
+using System.IO.Ports;
 
 namespace api_arduino.Services
 {
@@ -14,6 +15,32 @@ namespace api_arduino.Services
         public async Task<int> GetHumidity(string deviceId)
         {
             return 350;
+
+            // Remover return após testes
+            var device = CreateDevice(deviceId);
+
+            if (device.IsOpen is false)
+            {
+                try
+                {
+                    device.Open();
+                    device.Write("");
+                    device.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Erro ao se conectar com o dispositivo. {ex.InnerException}");
+                }
+            }
+        }
+
+        private SerialPort CreateDevice(string deviceId)
+        {
+            return new SerialPort
+            {
+                PortName = deviceId,
+                BaudRate = 9600
+            };
         }
     }
 }
